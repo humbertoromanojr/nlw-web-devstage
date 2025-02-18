62e86f2b2117b3a1551ac81a55467834c3780f02
humbertojr <astronomi@gmail.com>
2025-02-18 13:39:05 -0300

    Completed Button-icon component

--- src/app/components/button-icon.tsx
+++ src/app/components/button-icon.tsx
@@ -1,3 +1,13 @@
-export function ButtonIcon() {
-  return <button></button>;
+import { ReactNode } from "react";
+
+interface ButtonIconProps {
+  children: ReactNode;
+}
+
+export function ButtonIcon(props: ButtonIconProps) {
+  return (
+    <button className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-400 hover:bg-blue hover:text-gray-900">
+      {props.children}
+    </button>
+  );
 }

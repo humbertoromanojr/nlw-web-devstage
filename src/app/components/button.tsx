27f3c1271575378a27bc359e7428cc5bf8fc2500
humbertojr <astronomi@gmail.com>
2025-02-18 13:31:28 -0300

    Completed Button component

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,11 +1,13 @@
+import { ReactNode } from "react";
+
 interface ButtonProps {
-  text?: string;
+  children: ReactNode;
 }
 
 export function Button(props: ButtonProps) {
   return (
-    <button className="px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl">
-      {props.text || "Enviar"}
+    <button className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-400 hover:bg-blue hover:text-gray-900">
+      {props.children}
     </button>
   );
 }

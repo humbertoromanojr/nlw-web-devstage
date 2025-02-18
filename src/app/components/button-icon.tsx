5c4935ff986a86e111829b72522c285f6cf8a6bf
humbertojr <astronomi@gmail.com>
2025-02-18 13:49:00 -0300

    Extends all properties button

--- src/app/components/button-icon.tsx
+++ src/app/components/button-icon.tsx
@@ -1,13 +1,12 @@
-import { ReactNode } from "react";
+import { ComponentProps } from "react";
 
-interface ButtonIconProps {
-  children: ReactNode;
-}
+interface ButtonIconProps extends ComponentProps<"button"> {}
 
 export function ButtonIcon(props: ButtonIconProps) {
   return (
-    <button className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-400 hover:bg-blue hover:text-gray-900">
-      {props.children}
-    </button>
+    <button
+      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-400 hover:bg-blue hover:text-gray-900"
+      {...props}
+    />
   );
 }

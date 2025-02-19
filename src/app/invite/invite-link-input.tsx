852278d9139b3b31aece47e6dec3e0be779c459f
humbertojr <astronomi@gmail.com>
2025-02-19 12:16:53 -0300

    Passing parameters and use client

--- src/app/invite/invite-link-input.tsx
+++ src/app/invite/invite-link-input.tsx
@@ -1,21 +1,30 @@
+'use client'
+
 import { Copy, Link } from 'lucide-react'
 
 import { ButtonIcon } from '@/components/button-icon'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
 
-export function InviteLinkInput() {
+interface InviteLinkProps {
+  inviteLink: string
+}
+
+export function InviteLinkInput({ inviteLink }: InviteLinkProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink)
+
+    console.log('link copy!!!!', inviteLink)
+  }
+
   return (
     <InputRoot>
       <InputIcon>
         <Link className="size-5" />
       </InputIcon>
 
-      <InputField
-        readOnly
-        defaultValue="https://localhost:300/invite/3452345345324534534534"
-      />
+      <InputField readOnly defaultValue={inviteLink} />
 
-      <ButtonIcon className="-mr-2">
+      <ButtonIcon className="-mr-2" onClick={copyInviteLink}>
         <Copy className="size-5" />
       </ButtonIcon>
     </InputRoot>
--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -6,6 +6,8 @@ import { Ranking } from './ranking'
 import { Stats } from './stats'
 
 export default function Invite() {
+  const inviteLink = 'https://localhost:3000/invite/34523454565675676576'
+
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -30,7 +32,7 @@ export default function Invite() {
             </p>
           </div>
 
-          <InviteLinkInput />
+          <InviteLinkInput inviteLink={inviteLink} />
 
           <Stats />
         </div>
